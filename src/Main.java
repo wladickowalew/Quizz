15e19c113b0011dd7cb19be297436bfd76724166
Kovalev <test@gmail.ru>
2022-05-05 17:15:32 +0300

    add russian version

--- src/Main.java
+++ src/Main.java
@@ -52,8 +52,25 @@ public class Main {
         answers = new String[n];
         for (int i = 0; i < n; i++){
             JSONObject obj = array.getJSONObject(i);
-            answers[i] = (String) obj.get("answer");
-            questions[i] = (String) obj.get("question");
+            answers[i] = translate((String) obj.get("answer"));
+            questions[i] = translate((String) obj.get("question"));
         }
+    }
+
+    public static String translate(String word) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://google-translate1.p.rapidapi.com/language/translate/v2"))
+                .header("content-type", "application/x-www-form-urlencoded")
+                .header("Accept-Encoding", "application/gzip")
+                .header("X-RapidAPI-Host", "google-translate1.p.rapidapi.com")
+                .header("X-RapidAPI-Key", "0e0345dbc8msh8a0de13a6e012d1p1c4379jsn3e8c02db7303")
+                .method("POST", HttpRequest.BodyPublishers.ofString("q=" + word + "&target=ru&source=en"))
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        JSONObject obj = new JSONObject(response.body());
+        obj = (JSONObject) obj.get("data");
+        JSONArray arr = (JSONArray) obj.get("translations");
+        obj = (JSONObject) arr.get(0);
+        return (String) obj.get("translatedText");
     }
 }
